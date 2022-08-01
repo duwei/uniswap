a24662dc1c98dff9bb738407cb27920338bad13a
DuWei <duweiwork@gmail.com>
2022-08-01 23:01:28 +0900

    deploy uniswap contract

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x58EDd54b167723fB534257A540cAE29B99a3541F'
 
 // a list of tokens by chain
 type ChainTokenList = {
--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -89,9 +89,9 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', // v2 factory
-  '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D' // v2 router 02
+  '0xe5401dbf7fe1330e3bf3CF73543B800A0AC19075', // v2 factory
+  '0x6B4973cdBeDf3255BB928d68d687ea0B6569da38', // v2 router 01
+  '0x58EDd54b167723fB534257A540cAE29B99a3541F' // v2 router 02
 ]
 
 /**
